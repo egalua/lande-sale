3e48006c1d09c1bd88bbe3ffd2361d6ab0d9eb66
egalua <aybakaev@list.ru>
2020-12-13 17:34:47 +0300

    add key-properties block to index.js file

--- src/views/pages/index/index.js
+++ src/views/pages/index/index.js
@@ -1,5 +1,7 @@
 import './index.scss'
-import './blocks/main/gallery/images-list/images-list.js'
 import './blocks/header/top-nav/top-nav.js'
+import './blocks/main/gallery/images-list/images-list.js'
+import './blocks/main/key-properties/key-properties.js'
+
 
 console.log('index page');
