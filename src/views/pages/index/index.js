eaf24783b06f57f9aece793425953b37b894bb4f
egalua <aybakaev@list.ru>
2021-02-17 19:28:20 +0300

    add header module to index.js

--- src/views/pages/index/index.js
+++ src/views/pages/index/index.js
@@ -1,5 +1,5 @@
 import './index.scss'
-import './blocks/header/top-nav/top-nav.js'
+import './blocks/header/header.js'
 import './blocks/main/gallery/images-list/images-list.js'
 import './blocks/main/key-properties/key-properties.js'
 import './blocks/main/slider/slider.js'
